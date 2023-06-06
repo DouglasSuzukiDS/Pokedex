dc569742cdc2cfa22dd0a84c3fda3535b352285b
Doug <kaido19@outlook.com>
2023-06-05 23:48:19 -0300

    Autmating the APIto download the pokemon information, save in JSON & download SVGs

--- src/components/CardContainer/CardContainer.tsx
+++ src/components/CardContainer/CardContainer.tsx
@@ -1,16 +1,21 @@
 import { useEffect, useState } from 'react'
 import './CardContainer.css'
-import { MainClient, PokemonClient } from 'pokenode-ts'
-import fs from 'fs'
+import { PokemonClient } from 'pokenode-ts'
 
-//console.log(P)
+const PokemonAPI = new PokemonClient()
 
 // https://blog.curso-r.com/posts/2021-11-29-api-pokemon/#:~:text=O%20primeiro%20passo%20para%20acessar%20qualquer%20API%20é%20procurar%20uma%20documentação.&text=Isso%20significa%20que%20devemos%20fazer,(que%20chamamos%20de%20endpoint).
 // https://github.com/PokeAPI/pokeapi/issues/346
 // https://github.com/PokeAPI/sprites
 // https://github.com/PokeAPI/pokedex-promise-v2
-
+// https://pokenode-ts.vercel.app
+// https://pokemon.fandom.com/wiki/Poison_type
+// https://horadecodar.com.br/como-salvar-a-saida-do-console-log-em-arquivo/
+import Pokemons from '../../datas/Pokemons.json'
 import axios from 'axios'
+import imgTest from '../../assets/SVGs/359.svg'
+import inIMGS from '../../imgs/359.svg'
+
 
 export const CardContainer = () => {
    const [arrayPokemons, setArrayPokemons]: any[] = useState([])
@@ -18,8 +23,11 @@ export const CardContainer = () => {
    const pokedex = new PokemonClient()
    
    useEffect(() => {
-      getPokemonsList()
-   }, [])
+      // getPokemonsList()
+      // downloadPokemonSVG(pichuAPI)
+      // downloadPokemonInfos('pichu')
+      findPokemonById(639)
+   })
 
    const getPokemonsList = async() => { 
       await pokedex.listPokemons()
@@ -31,39 +39,89 @@ export const CardContainer = () => {
          .catch(e => console.log(e))
       })
       .catch(e => console.log(e))
-   } 
+   }
 
-   const pokemonByName = async(name: string) => {
-      await pokedex.getPokemonByName(`${name}`)
-         .then(res => console.log(res))
-         // .then(res => res.json())
-         .then(async(res) => {
-            await fs.writeFile('../../datas/aa.json', res, (err) => {
-               if(err) {
-                  console.log('err')
-               } else {
-                  console.log('ok')
-               }
+   const downloadPokemonSVG = async(obj: any) => {
+
+      const svg = obj.sprites.other.dream_world.front_default
+      if(svg !== null) {
+         // console.log(svg)
+         
+         await axios.get(svg)
+            .then(res => {
+               console.log(res)
+               const blob = new Blob([res.data], {type: 'image/svg+xml'});
+               const link = document.createElement('a');
+               link.href = window.URL.createObjectURL(blob);
+               link.download =`${obj.id}.svg`;
+               link.click();
+               // console.log(res)
             })
+            .catch(e => console.log(e))
+      } 
+      /*else {
+         const officialArtwork = obj.sprites.other.home.front_default
+         console.log(officialArtwork)
+
+         await axios.get(officialArtwork)
+            .then(res => {
+               console.log(res)
+               const blob = new Blob([res], {type: 'image/png'});
+               const link = document.createElement('a');
+               link.href = window.URL.createObjectURL(blob);
+               link.download =`${obj.id}.svg`;
+               link.click();
+               console.log(res)
+            })
+            .catch(e => console.log(e))
+      }*/        
+   }
+
+   const downloadPokemonInfos = async(name: string) => {
+      await pokedex.getPokemonByName(`${name}`)
+         // .then(res => console.log(res))
+         .then(res => {
+            const data = {
+               name: res.name,
+               id: res.id,
+               type: res.types,
+               height: res.height,
+               weight: res.weight,
+               base_experience: res.base_experience,
+               sprites: res.sprites
+            }
+
+            const blob = new Blob([JSON.stringify(data)], {type: "application/json"});
+            const link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download =`${name}.json`;
+            link.click();
+         })
+
+         .catch(e => console.log(e))
+   }
+
+   const findPokemonById  = async(id: number) => {
+      await PokemonAPI.getPokemonById(id)
+         .then(res => {
+            console.log(res)
          })
          .catch(e => console.log(e))
    }
 
-
-   // res.data.sprites.other.dream_world.front_default
-
-   {/* { setArrayPokemonsSVG.map((svg: any) => {
-                           <img src={svg} alt={el.name} />
-                        }) } */}
-                        // { 
-                        //    await axios.get(`${ el.url }`)
-                        //       .then(res => setArrayPokemonsSVG(res.data.sprites.other.dream_world.front_default))
-                        //       .catch(e => console.log(e))
-                        // }
+   const pokImg = Pokemons.pokemons[0].imgs
+  
+   console.log(inIMGS)
+   console.log(Pokemons.pokemons[0].imgs)
+   console.log(pokImg)
 
    return (
       <>
          <section className="cardContainer font-bold flex flex-wrap text-pokemon overflow-y-auto">
+            <img src={inIMGS} alt="Via Import" />
+            {/* <img src={Pokemons.pokemons[0].imgs} alt="Via Json" />
+            <img src={pokImg} alt="Via Const" /> */}
+               
             <ul>    
                { arrayPokemons.map((el:any) => 
                  <li key={ el.name }>
